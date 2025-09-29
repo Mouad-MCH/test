c0af720775254f62d2dab888af72c39c37a85495
Mouad charadi <moadchradi12@gmail.com>
2025-09-29 09:41:28 +0200

    Modify readme

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("test");
+// const prompt = require("prompt-sync")();
+
+// let x = prompt("Ecrire un nombre de lignes : ");
+let x = 10
+
+for(let i = 1; i<=x; i++) {
+  let b = "*";
+  for(let j = 1; j<=x; j+=2) {
+    console.log(b.repeat(j));
+  }
+}
