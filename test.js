8bc2fe30a1335d2169118830573f13d01f20727c
Mouad charadi <moadchradi12@gmail.com>
2025-10-18 21:18:03 +0200

    add test and thing file

--- test.js
+++ test.js
@@ -1,11 +1,20 @@
 // const prompt = require("prompt-sync")();
 
 // let x = prompt("Ecrire un nombre de lignes : ");
-let x = 10
 
-for(let i = 1; i<=x; i++) {
-  let b = "*";
-  for(let j = 1; j<=x; j+=2) {
-    console.log(b.repeat(j));
+
+function keyValuePrinter(obj) {
+  for(let key in obj) {
+    if(typeof obj[key] !== "object") {
+      console.log("[" + key + " : " + obj[key] + "]");
+    }else {
+      keyValuePrinter(obj[key])
+    }
   }
 }
+
+let obj = {
+  nom:"name",
+  type:"type",
+}
+keyValuePrinter(obj)
